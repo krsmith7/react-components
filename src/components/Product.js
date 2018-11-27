409b2f00e81d796299abdf97bae05bd7be423f70
Katricia Smith <smithkatricia@gmail.com>
2018-11-27 12:27:13 -0800

    Add classNames to Product component return

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,21 +2,17 @@ import React from 'react';
 import './Product.css';
 import PropTypes from 'prop-types';
 
-// Product
-// - takes name
-// - takes a product photo URL
-// - takes a standard price
-// - a discount price.
 
 // If the discounted price is less than the standard price then the component should display the discounted price with some visual indicator (e.g. the price text is red).
 
 const Product = (props) => {
+
   return (
     <section className="product">
-      <div>{props.name}</div>
-      <div>{props.photoUrl}</div>
-      <div>{props.standardPrice}</div>
-      <div>{props.discountPrice}</div>
+      <div className="product-name">{props.name}</div>
+      <div className="product-photo">{props.photoUrl}</div>
+      <div className="product-price">{props.standardPrice}</div>
+      <div className="product-discount">{props.discountPrice}</div>
     </section>
   );
 };
